d9c2772c12eafc4def9c49739b4d59b944789d26
codeshallow <1272178260@qq.com>
2022-04-02 14:18:30 +0800

    权限菜单管理初步

--- src/main/java/com/qianyu/springboot/controller/MenuController.java
+++ src/main/java/com/qianyu/springboot/controller/MenuController.java
@@ -2,12 +2,14 @@ package com.qianyu.springboot.controller;
 
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.qianyu.springboot.common.Result;
 import org.springframework.web.bind.annotation.*;
+
 import javax.annotation.Resource;
 import java.util.List;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.qingge.springboot.common.Result;
+import java.util.stream.Collectors;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.qianyu.springboot.service.IMenuService;
 import com.qianyu.springboot.entity.Menu;
 
@@ -15,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author 浅羽
@@ -23,47 +25,61 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/menu")
-        public class MenuController {
-    
-@Resource
-private IMenuService menuService;
+public class MenuController {
 
-// 新增或者更新
-@PostMapping
-public Result save(@RequestBody Menu menu) {
-    menuService.saveOrUpdate(menu);
+    @Resource
+    private IMenuService menuService;
+
+    // 新增或者更新
+    @PostMapping
+    public Result save(@RequestBody Menu menu) {
+        menuService.saveOrUpdate(menu);
         return Result.success();
-        }
+    }
 
-@DeleteMapping("/{id}")
-public Result delete(@PathVariable Integer id) {
-    menuService.removeById(id);
+    @DeleteMapping("/{id}")
+    public Result delete(@PathVariable Integer id) {
+        menuService.removeById(id);
         return Result.success();
-        }
+    }
 
-@PostMapping("/del/batch")
-public Result deleteBatch(@RequestBody List<Integer> ids) {
-    menuService.removeByIds(ids);
+    @PostMapping("/del/batch")
+    public Result deleteBatch(@RequestBody List<Integer> ids) {
+        menuService.removeByIds(ids);
         return Result.success();
-        }
+    }
 
-@GetMapping
-public Result findAll() {
-        return Result.success(menuService.list());
-        }
-
-@GetMapping("/{id}")
-public Result findOne(@PathVariable Integer id) {
-        return Result.success(menuService.getById(id));
-        }
-
-@GetMapping("/page")
-public Result findPage(@RequestParam Integer pageNum,
-@RequestParam Integer pageSize) {
+    @GetMapping
+    public Result findAll(@RequestParam(defaultValue = "") String name) {
         QueryWrapper<Menu> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("name",name);
+        //查询所有数据
+        List<Menu> list = menuService.list(queryWrapper);
+        //找出pid为null的一级菜单
+        List<Menu> parentNode = list.stream().filter(menu -> menu.getPid() == null).collect(Collectors.toList());
+        //找出一级菜单的子菜单
+        for (Menu menu : parentNode) {
+            //筛选所有数据中pid=父级id的数据就是二级菜单
+            menu.setChildren(list.stream().filter(m -> menu.getId().equals(m.getPid())).collect(Collectors.toList()));
+
+        }
+        return Result.success(parentNode);
+    }
+
+    @GetMapping("/{id}")
+    public Result findOne(@PathVariable Integer id) {
+        return Result.success(menuService.getById(id));
+    }
+
+    @GetMapping("/page")
+    public Result findPage(@RequestParam String name,
+                           @RequestParam Integer pageNum,
+                           @RequestParam Integer pageSize) {
+        QueryWrapper<Menu> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("name",name);
         queryWrapper.orderByDesc("id");
         return Result.success(menuService.page(new Page<>(pageNum, pageSize), queryWrapper));
-        }
+    }
 
-        }
+}
 
--- src/main/java/com/qianyu/springboot/entity/Menu.java
+++ src/main/java/com/qianyu/springboot/entity/Menu.java
@@ -1,17 +1,21 @@
 package com.qianyu.springboot.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+
 import java.io.Serializable;
+import java.util.List;
+
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
-    
+
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author 浅羽
@@ -19,27 +23,33 @@ import lombok.Setter;
  */
 @Getter
 @Setter
-    @TableName("sys_menu")
+@TableName("sys_menu")
 @ApiModel(value = "Menu对象", description = "")
 public class Menu implements Serializable {
 
-private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-                            @ApiModelProperty("id")
-                                        @TableId(value = "id", type = IdType.AUTO)
-                                    private Integer id;
+    @ApiModelProperty("id")
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
 
-                        @ApiModelProperty("名称")
-                                private String name;
+    @ApiModelProperty("名称")
+    private String name;
 
-                        @ApiModelProperty("路径")
-                                private String path;
+    @ApiModelProperty("路径")
+    private String path;
 
-                        @ApiModelProperty("图标")
-                                private String icon;
+    @ApiModelProperty("图标")
+    private String icon;
 
-                        @ApiModelProperty("描述")
-                                private String description;
+    @ApiModelProperty("描述")
+    private String description;
+
+    //在数据库表格里没有
+    @TableField(exist = false)
+    private List<Menu> children;
+
+    private Integer pid;
 
 
-        }
+}
